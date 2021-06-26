6168ba8978a47dd3c4c245e8482761e0529e2864
Kayisabully <86518040+Kayisabully@users.noreply.github.com>
2021-06-26 18:38:44 +0100

    Add files via upload

---
+++ pogressBar/client.lua
@@ -0,0 +1,36 @@
+
+
+]]--
+
+function drawBar(time, text, cb)
+	SendNUIMessage({
+		time = time,
+		text = text,
+	})
+	if cb then
+		Citizen.SetTimeout(time + 100, cb)
+	end
+end
+
+RegisterNetEvent('pogressBar:drawBar')
+AddEventHandler('pogressBar:drawBar', function(time, text, cb)
+	drawBar(time, text, cb)
+end)
+
+--[[
+
+Examples
+
+Citizen.CreateThread(function()
+
+	drawBar(5000, 'FeelsGoodMan', function()
+		print('callback Test')
+	end)
+
+
+	TriggerEvent('pogressBar:drawBar', 1000, 'test', function()
+		print('callback Test')
+	end)
+end)
+
+]]--
